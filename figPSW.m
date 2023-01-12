81c5b55192866b2a63f9a667857a83d53b573717
Piotr Wawryka <piotrwpwsz@gmail.com>
2023-01-12 18:46:24 +0100

    auto arange window

--- figPSW.m
+++ figPSW.m
@@ -5,7 +5,9 @@ if ~isfile('figPW.m')
  urlwrite ('https://raw.githubusercontent.com/informacja/MTF/main/figPW.m', 'figPW.m');
 end
 
+figpos = {'north','south','east','west','northeast','northwest','southeast','southwest','center','onscreen'};
 for i = 1:length(h2)
     figure(h2(i))
+    movegui(cell2mat( figpos(mod(i,length(figpos)-1)+1) ));
     figPW;
 end
